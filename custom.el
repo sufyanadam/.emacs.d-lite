7a871373a9b97ac3f3d72432072c291028678f50
sufyanadam <sufyan.adam@gmail.com>
2014-01-14 22:32:12 -0800

    css indent is 2 spaces

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
  '(custom-safe-themes (quote ("8bb1e9a22e9e9d405ca9bdf20b91301eba12c0b9778413ba7600e48d2d3ad1fb" "d6a00ef5e53adf9b6fe417d2b4404895f26210c52bb8716971be106550cea257" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
