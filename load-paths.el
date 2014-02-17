8ef3ac02c2c117e73bf179b5511b752dc931e38c
sufyanadam <sufyan.adam@gmail.com>
2014-02-17 12:12:15 -0800

    better tab completion in shell-mode

--- load-paths.el
+++ load-paths.el
@@ -1,5 +1,9 @@
+(setq site-lisp-dir
+      (expand-file-name "site-lisp" user-emacs-directory))
+
 ;; Set up load path
 (add-to-list 'load-path user-emacs-directory)
+(add-to-list 'load-path site-lisp-dir)
 
 ;el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
