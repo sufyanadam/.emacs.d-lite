aa498c7584580b9e8282a6054f84f5ce242c881d
sufyanadam <sufyan.adam@gmail.com>
2014-03-03 21:43:00 -0800

    sensible defaults for org, user should set own file preferences

--- setup-org.el
+++ setup-org.el
@@ -11,8 +11,6 @@
 (defadvice kill-whole-line (after fix-cookies activate)
   (myorg-update-parent-cookie))
 
-(setq org-directory "~/Dropbox/org")
-(setq org-default-notes-file (concat org-directory "/notes.org"))
 (setq org-log-done t)
 (define-key global-map (kbd "M-<f6>") 'org-capture)
 (define-key global-map "\C-cl" 'org-store-link)
---
+++ user/sufyan/org-settings.el
@@ -0,0 +1,3 @@
+(setq org-directory "~/Dropbox/org")
+(setq org-agenda-files (quote ("~/Dropbox/org/agenda")))
+(setq org-default-notes-file (concat org-directory "/notes.org"))
